c07cfb5751c9c8c877d2f52abeeecb5dfbd6f14a
hannahsshh <hannah.shusterhyman@mail.utoronto.ca>
2025-10-04 11:58:17 -0400

    Adjustments to assingment to fix moving files to raw directory

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -24,6 +24,7 @@ touch analysis/main.py
 curl -Lo rawdata.zip https://github.com/UofT-DSI/shell/raw/refs/heads/main/02_activities/assignments/rawdata.zip
 unzip -q rawdata.zip
 
+
 ###########################################
 # Complete assignment here
 
@@ -31,14 +32,15 @@ unzip -q rawdata.zip
 mkdir data
 
 # 2. Move the ./rawdata directory to ./data/raw
+# Self note: are we meant to keep "rawdata" as its own directory, or have it just become "raw" (keep the directory within raw or just have the files themselves in raw)
 
 mkdir data/raw
 
-mv rawdata data/raw
+mv rawdata/* data/raw
 
 # 3. List the contents of the ./data/raw directory
 
-ls data/raw/rawdata
+ls data/raw
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
 
@@ -50,29 +52,29 @@ cd processed
 
 mkdir "server_logs" "user_logs" "event_logs"
 
-
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 
 cd ..
 
-cp raw/rawdata/server*.log processed/server_logs
+cp raw/server*.log processed/server_logs
 
 # 6. Repeat the above step for user logs and event logs
 
-cp raw/rawdata/event*.log processed/event_logs
+cp raw/event*.log processed/event_logs
 
 # Self note: did not move over the user logs with the IP attached -- is this correct??
-cp raw/rawdata/user_log*.log processed/user_logs
+cp raw/user_log*.log processed/user_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 
 # Self note: removing files with IP address only from raw folder and not from user logs because didn't move any of those IP containing logs above
-rm raw/rawdata/*ipaddr*.* 
+rm raw/*ipaddr*.* 
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 
 touch "inventory.txt"
 ls processed/*/ >> "inventory.txt"
+
 
 ###########################################
 
